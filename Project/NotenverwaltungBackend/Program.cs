5757280d7fe83fa145db671d8b998eac32ec47d5
Irame <irame94@gmail.com>
2017-07-14 14:55:14 +0200

    changed listening urls to "http://*:5000" and "https://*:5001"

--- Project/NotenverwaltungBackend/Program.cs
+++ Project/NotenverwaltungBackend/Program.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
 using NotenverwaltungBackend.Controllers;
 using NotenverwaltungBackend.Data;
 
@@ -14,8 +15,16 @@ namespace NotenverwaltungBackend
     {
         public static void Main(string[] args)
         {
+            var config = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("hosting.json", optional: true)
+                .AddEnvironmentVariables(prefix: "ASPNETCORE_")
+                .Build();
+            
             var host = new WebHostBuilder()
                 .UseKestrel()
+                .UseUrls("http://*:5000", "https://*:5001")
+                .UseConfiguration(config)
                 .UseContentRoot(Directory.GetCurrentDirectory())
                 .UseIISIntegration()
                 .UseStartup<Startup>()
